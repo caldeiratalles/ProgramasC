848ee22e506be0a1a74ff7fdae532df990dddaed
TM <64755991+caldeiratalles@users.noreply.github.com>
2020-12-15 18:56:06 -0300

    Add files via upload
    Front-end muito inicial.

---
+++ Readme.txt
@@ -0,0 +1,3 @@
+Imagens de autoria do git https://github.com/juliomoreira666/site-do-zero
+
+Front-end inicial de um site.
---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 350,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+});
